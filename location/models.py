bc3da6e8b06b6e37269bac1e0a7594341b27dd84
Chauhan Laxmi Santosh <138270844+laxmie124@users.noreply.github.com>
2024-03-16 17:12:14 +0530

    Add files via upload

---
+++ location/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class State(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class District(models.Model):
+    state = models.ForeignKey('location.State', related_name='districts', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.state} -- {self.name}'
+
+
+class Locality(models.Model):
+    district = models.ForeignKey('location.District', related_name='localities', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    medical_agency = models.ForeignKey('medical.MedicalAgency', related_name='localities', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.district} -- {self.name}'
+
+    class Meta():
+        verbose_name_plural = 'Localities'
